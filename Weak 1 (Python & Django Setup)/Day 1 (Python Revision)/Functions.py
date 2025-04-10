7ecdabdab21c19235943448b8769fce600b10a64
Developer-Shafi <shafitechdeveloper1133@gmail.com>
2025-04-10 23:57:17 +0500

    Still working on functions

--- Weak 1 (Python & Django Setup)/Day 1 (Python Revision)/Functions.py
+++ Weak 1 (Python & Django Setup)/Day 1 (Python Revision)/Functions.py
@@ -1,1 +1,14 @@
+# Functions in Python 
 print("Functions in Python")
+def greet(name = "Ahsan"):
+    return f"How are you? ,{name}!"
+
+print(greet("Ahsan"))
+
+def Result(name):
+    return f"Your Current CGPA is 3.5 {name}"
+print(Result("Shafiullah"))
+
+
+
+
