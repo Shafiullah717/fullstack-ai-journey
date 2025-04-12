e2b5cff715c14a3122a136837f4c22c87282146e
Developer-Shafi <shafitechdeveloper1133@gmail.com>
2025-04-12 10:51:10 +0500

    Revise Encapsulation

--- Weak 1 (Python & Django Setup)/Day 1 (Python Revision)/Inheritance and Encapsulation.py
+++ Weak 1 (Python & Django Setup)/Day 1 (Python Revision)/Inheritance and Encapsulation.py
@@ -1,4 +1,4 @@
-print("Inheritance and Encapsulation in python")
+# print("Inheritance and Encapsulation in python")
 # Inheritance allow a child class to inherit attributes and metthods for the parent class.
 
 # Parent class
@@ -52,5 +52,63 @@ class SavingsAccount(BankAccount):
         self.deposit(interest)  # Reuse parent's deposit() method
 
 # Create a savings account
-alice_savings = SavingsAccount("Alice", 1000)
-alice_savings.add_interest() 
+# alice_savings = SavingsAccount("Alice", 1000)
+# alice_savings.add_interest() 
+
+#  Encapsulation restrict direct access to an object's internal data and exposes only what's necessary.
+
+# private Attributes
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.__age = age  # Private attribute (double underscore)
+
+    def get_age(self):    # Getter method
+        return self.__age
+
+    def set_age(self, age):  # Setter method (with validation)
+        if age > 0:
+            self.__age = age
+        else:
+            print("Age must be positive!")
+
+# alice = Person("Alice", 30)
+# print(alice.get_age()) 
+# # alice.set_age(-5)   
+# Hayatullah = Person("Hayatullah", 17)
+# print(Hayatullah.get_age())
+# Hayatullah.set_age(18)
+# print(Hayatullah.get_age())
+# Shafiullah = Person("Shafiullah" , 22)
+# print(Shafiullah.get_age())
+# Shafiullah.__age = 31
+# print(Shafiullah.__age)
+# Ahsan = Ahsan.__age = 22
+# print(Ahsan)
+
+# Python's @property Decorator
+
+class Temperature:
+    def __init__(self, celsius):
+        self._celsius = celsius  # Protected attribute
+
+    @property
+    def celsius(self):  # Getter
+        return self._celsius
+
+    @celsius.setter
+    def celsius(self, value):  # Setter with validation
+        if value < -273.15:
+            raise ValueError("Temperature below absolute zero!")
+        self._celsius = value
+
+    @property
+    def fahrenheit(self):  # Computed property
+        return (self._celsius * 9/5) + 32
+
+temp = Temperature(25)
+print(temp.fahrenheit)  
+temp.celsius = 30       
+temp.celsius = -200     
+print(temp.celsius)
