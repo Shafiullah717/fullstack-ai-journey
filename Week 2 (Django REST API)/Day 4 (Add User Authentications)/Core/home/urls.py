ddd21f55967e3db7c616131fafc4182b6c8ee96e
Developer-Shafi <shafitechdeveloper1133@gmail.com>
2025-04-25 20:46:50 +0500

    Add user Authentication

---
+++ Week 2 (Django REST API)/Day 4 (Add User Authentications)/Core/home/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('student/', StudentApi.as_view(), name='student_api'),
+    path('register/', RegisterUser.as_view(), name='register_user'),
+]
